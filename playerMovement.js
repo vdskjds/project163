5ed213b22306cd1eeace5c2f1b3585425ac4eb1b
vdskjds <142517684+vdskjds@users.noreply.github.com>
2024-02-10 21:34:56 +0530

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("player-movement", {
+    init: function () {
+      this.walk();
+    },
+    walk: function () {
+      window.addEventListener("keydown", (e) => {
+        // Add the condition to play sound
+
+
+
+      });
+    },
+  });
+
+
+  
